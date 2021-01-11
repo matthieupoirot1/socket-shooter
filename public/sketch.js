52fb7b5102056579c867e7c5418b385ab8aa0036
Matthieu Poirot <matthieu.poirot1@gmail.com>
2021-01-11 11:00:53 +0100

    clean sketch and buff update

--- public/sketch.js
+++ public/sketch.js
@@ -1,45 +1,58 @@
-const socket = io.connect('localhost:23000');
 const SPACEBAR = 32;
+
+const serverSocket = io('localhost:23000');
+
+serverSocket.on('connect_error', err => console.log(err));
+serverSocket.on('connect_failed', err => console.log(err));
 
 let players = [];
 let projectiles = [];
 
-socket.on("heartbeat", (serverElements) => {
-  players = serverElements.players.map((serverPlayer) => new Player(serverPlayer));
-  projectiles = serverElements.projectiles.map((serverProjectile)=> new Projectile(serverProjectile));
-  console.log(projectiles);
-});
-
-socket.on("disconnect", playerId => removePlayer(playerId));
-
 function setup() {
+  frameRate(60);
   createCanvas(windowWidth, windowHeight);
+
+  serverSocket.on('connect', function() {
+    console.log("Connected to server socket !");
+  });
+
+  serverSocket.on("heartbeat", (serverElements) => {
+    //replace and reconstruct players
+    players = serverElements.players.map((serverPlayer) => new Player(serverPlayer));
+    //replace and reconstruct projectiles
+    projectiles = serverElements.projectiles.map((serverProjectile)=> new Projectile(serverProjectile));
+  });
+
+  serverSocket.on("disconnect", playerId => removePlayer(playerId));
 }
 
 function draw() {
   handlePlayerMovement();
+  //remove everything
   clear();
   background(220);
+  //draw every players
   players.forEach(player => player.draw());
+  //draw every projectiles
   projectiles.forEach(projectile => projectile.draw());
 }
 
 function mouseClicked(){
-    socket.emit("shoot",{mouseX : mouseX, mouseY: mouseY});
+    serverSocket.emit("shoot",{mouseX : mouseX, mouseY: mouseY});
 }
 
 function handlePlayerMovement(){
   if (keyIsDown(81)) {
-    socket.emit('move',{axis:"-x"});
+    serverSocket.emit('move',{axis:"-x"});
   }
   if (keyIsDown(68)) {
-    socket.emit('move',{axis:"x"});
+    serverSocket.emit('move',{axis:"x"});
   }
   if (keyIsDown(90)) {
-    socket.emit('move',{axis:"-y"});
+    serverSocket.emit('move',{axis:"-y"});
   }
   if (keyIsDown(83)) {
-    socket.emit('move',{axis:"y"});
+    serverSocket.emit('move',{axis:"y"});
   }
 }
 
---
+++ server/Buff.js
@@ -0,0 +1,43 @@
+
+class Buff {
+    constructor(socketId) {
+        /** @type {number} */
+        this.type = Math.floor(Math.random() * Math.floor(4));
+        /** @type {number} */
+        this.x = Math.random() * 400 + 1;
+        /** @type {number} */
+        this.y = Math.random() * 400 + 1;
+        /** @type {string} */
+        this.id = socketId;
+        /** @type {{r: number, b: number, g: number}}*/
+        this.rgb = {
+            r: Math.random() * 255,
+            g: Math.random() * 255,
+            b: Math.random() * 255,
+        }
+    }
+
+    /**
+     * Listener to handle "move" event received from server
+     * @param moveObject "move" event information received through client socket
+     * @param {string} moveObject.axis axis received to increase corresponding position
+     */
+    handlePlayerMovement(moveObject) {
+        switch (moveObject.axis) {
+            case "x":
+                this.x += 5;
+                break;
+            case "-x":
+                this.x -= 5;
+                break;
+            case "y":
+                this.y += 5;
+                break;
+            case "-y":
+                this.y -= 5;
+                break;
+        }
+    }
+}
+
+module.exports = Player;
